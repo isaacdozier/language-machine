c1ee560e5826d3cf898676e6be19edd856edb8b4
Isaac Dozier <isaactdozier@gmail.com>
2016-06-09 03:20:22 +0000

    fix stack issue - app is running

--- src.js
+++ src.js
@@ -1,66 +1,44 @@
 const fs  = require('fs');
-const fn  = 'data.txt', fw  = 'words.txt';
-var tws = fs.readFileSync(fw,'utf8').toString().split('\n');
-var tmp = fs.readFileSync(fn,'utf8').toString().split('\n');
-var stm = fs.createWriteStream(fn);
+const dataFile   = 'data.txt', 
+      wordsFile  = 'words.txt';
+      
+var words = fs.readFileSync(wordsFile,'utf8').toLowerCase().toString().split('\n');
 
-//test
-//////
-
-function lrn(cnt){
-  var m;
-  var n = cnt+1;
-
-  for(var i=0;i<tmp.length;i++){
-    if(tws[cnt] && tws[cnt].toLowerCase().includes(tmp[i])){
-      if(i===0){m=0;} else{m=1;} 
-      tmp.splice(i-m,0,tmp.splice(i,1));  
-    }
-  }
-
-  fs.writeFile(fn, tmp.join('\n'), (err) => {
-    if (err) throw err;
-  });
-
-  console.log(cnt+'::'+tws[cnt]);
-
-  if (cnt < 5000) {
-    process.nextTick(function(){
-      lrn(n);
-    });    
-  } 
-}
-
-lrn(2000);
-
-
-
-
-
-function learn(cnt){
-  var m;
-  var n = cnt + 1;
-  var wrd = tws[cnt].toLowerCase();
-  var stream = fs.createWriteStream("data.txt");
-  stream.once('open', function(fd) {
-    for(var i=0;i<tmp.length;i++){
-      if(wrd.includes(tmp[i])){
-        if(i===0){m=0;}else{m=1;}
-        tmp.splice(i-m,0,tmp.splice(i,1));
+//call to learn language
+function learn(count){
+  fs.readFile(dataFile, 'utf8', function(error, data) {
+    var newData = data.replace(/\r\n/g,'\n').split('\n');
+    
+    for(var i in newData){
+      var noticed = words[count].indexOf(newData[i]) > -1;
+      var notTop  = i > 0;
+      var flipWords, movePos;
+      
+      if(noticed && notTop){
+        flipWords = newData[i]+'\r\n'+newData[i-1];
+        movePos = i - 1;
+        
+        newData.splice(i-1,2);
+        newData.splice(movePos,0,flipWords)
       }
     }
-    stream.write(tmp.join('\n'));
-    if (cnt < 100) {
-      process.nextTick(function() { 
-        learn(n) 
-      });
-    }
-    stream.end();
+    
+    fs.writeFile(dataFile, newData.join('\n'), (err) => {
+      if (err) throw err;
+      
+      console.log(count+'::'+words[count]);
+      
+      process.nextTick(function(){
+        learn(count+1);
+      });    
+    });
   });
-  
-  console.log(cnt+'::'+tws[cnt]);
 }
 
+learn(0);
+
+
+//call to build data.txt file
 function vData(){
   var cns = 'bcdfghjklmnpqrstvwxz'.split('');
   var vls = 'aeiouy'.split('');
